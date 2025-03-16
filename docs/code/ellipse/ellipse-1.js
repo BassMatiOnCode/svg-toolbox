e50abc9f1aad17c09403350d72b7a1d77595fc99
BassmatiOnCode <75748577+BassMatiOnCode@users.noreply.github.com>
2025-03-16 19:58:51 +0100

    Class Ellipse added.
    On branch Ellipse
    Changes to be committed:
    	modified:   code/ellipse/ellipse-1.js
    	typechange: code/ellipse/ellipse.js
    	new file:   ellipse/demo-01.htm
    	new file:   ellipse/ellipse.htm
    	modified:   toc.htm

--- docs/code/ellipse/ellipse-1.js
+++ docs/code/ellipse/ellipse-1.js
@@ -1,6 +1,76 @@
-﻿// 2025-03-15
-// Requires: svg.js
-function Ellipse( x, y, rx=10, ry=10, attributes={ }, parent ) {
+﻿// 2025-03-16
+
+import { addSvgElement, setProperties } from "../core/core.js" ;
+
+export class Ellipse {
+	#a; #b; #c; #e; #orientation; #x; #y; #rx; #ry; #element;
+/**
+ *	ctor( ) 
+*/	constructor ( { p, x, y, rx, ry, fromEllipse, attributes={ }, options={ } } ) {
+		if ( fromEllipse ) {
+			if ( typeof fromEllipse === "string" ) fromEllipse = document.getElementById( fromEllipse );
+			p = undefined ;
+			x = fromEllipse.cx.baseVal.value ;
+			y = fromEllipse.cy.baseVal.value ;
+			rx = fromEllipse.rx.baseVal.value ;
+			ry = fromEllipse.ry.baseVal.value ;
+			this.#element = fromEllipse ;
+			}
+		if ( p !== undefined ) { x = p.x ; y = p.y }
+		this.#x = x ;
+		this.#y = y ;
+		this.#rx = rx ;
+		this.#ry = ry ;
+		if ( ! this.#element ) this.#element = addSvgElement( "ellipse", setProperties( { cx: x, cy: y, rx: rx, ry: ry }, attributes ), { }, options );
+		}
+/**
+ *	calculateOrientation ( )
+*/	#calculateOrientation ( ) {
+		if ( this.#rx < this.#ry ) { this.#a = this.#ry ; this.#b = this.#rx ; this.#orientation = "vertical" }
+		else { this.#a = this.#rx ; this.#b = this.#ry ; this.#orientation = "horizontal" }
+		this.#c = Math.sqrt( this.#a ** 2 - this.#b ** 2 );
+		this.#e = this.#c / this.#a;
+		}
+/** get orientation 
+*/	get orientation ( ) { 
+		if ( this.#orientation === undefined ) this.#calculateOrientation( );
+		return this.#orientation ; 
+		}
+/**
+ *	get a ( )
+*/	get a ( ) { 
+		if ( this.#a === undefined ) this.#calculateOrientation( );
+		return this.#a ;
+		}
+/**
+ *	get b ( )
+*/	get b ( ) { 
+		if ( this.#b === undefined ) this.#calculateOrientation( );
+		return this.#b ;
+		}
+/**
+ *	get c ( ) Linear excentricity, distance of focal points from center
+*/	get c ( ) { 
+		if ( this.#c === undefined ) this.#calculateOrientation( );
+		return this.#c ;
+		}
+/**
+ *	get e ( ) Numeric excentricity
+*/	get e ( ) { 
+		if ( this.#e === undefined ) this.#calculateOrientation( );
+		return this.#e ;
+		}
+/** get x */	get x ( ) { return this.#x }
+/** set x */	set x ( value ) { this.#x = value ; return this }
+/** get y */	get y ( ) { return this.#y }
+/** set y */	set y ( value ) { this.#y = value ; return this }
+/** get rx */	get rx ( ) { return this.#rx }
+/** set rx */	set rx ( value ) { this.#rx = value ; return this }
+/** get ry */	get ry ( ) { return this.#ry }
+/** set ry */	set ry ( value ) { this.#ry = value ; return this }
+	}
+
+function xEllipse( x, y, rx=10, ry=10, attributes={ }, parent ) {
 	// Creates an ellipse object
 	this.o = addSvgElement( "ellipse", setProperties( attributes, { x: x, y: y, rx: rx, ry: ry } ), { }, parent );
 	// Center point
@@ -15,17 +85,17 @@ function Ellipse( x, y, rx=10, ry=10, attributes={ }, parent ) {
 	this.nx = this.c / this.a;
 	}
 
-Ellipse.prototype.getPointAt = function ( { x, y } ) {
+xEllipse.prototype.getPointAt = function ( { x, y } ) {
 	if ( y === undefined ) return Math.sqrt( this.b ** 2 * ( 1 - x ** 2 / this.a ** 2 )) ; 
 	else return Math.sqrt( a ** 2 * ( 1 - y ** 2 / this.b ** 2 ));
 	}
 
-Ellipse.prototype.tangentAt = function ( x0, y0, { x, y }={ } ) {
+xEllipse.prototype.tangentAt = function ( x0, y0, { x, y }={ } ) {
 	if ( y === undefined ) return y0 - x0 / y0 * b ** 2 / a ** 2 * (x - x0 ) ;
 	else return x0 - y0 / x0 * a ** 2 / b ** 2 * (y - y0 );
 	}
 
-Ellipse.prototype.normalAtNOTOK = function ( x0, y0, { x, y }={ } ) {
+xEllipse.prototype.normalAtNOTOK = function ( x0, y0, { x, y }={ } ) {
 	if ( x !== undefined ) return { x: ellipse.tangentAt( x0, y0, { x: x } ) - y0 + x0, y: x0 - x + y0 };
 	else if ( y !== undefined ) {
 		const y1 = y ;
